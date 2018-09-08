954919a2f87e39f813e4749bc4b31171d819c20f
david m flynn <flynndave1@msn.com>
2018-09-08 00:47:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 const currentUser = 'David Flynn';
 
-const welcomeMessage = 'Welcome to Flatbook, ';
+const welcomeMessage = 'Welcome to Flatbook, currentUser';
 
-const welcomeMessage = currentUser;
+
