a0870453c1511c64b76630679416119b81e3d47d
david m flynn <flynndave1@msn.com>
2018-09-08 01:17:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome, ${currentUser.substring(0,1)}`;
+const shortGreeting = `Welcome, ${currentUser.substring(0,1)}!`;
+
 
